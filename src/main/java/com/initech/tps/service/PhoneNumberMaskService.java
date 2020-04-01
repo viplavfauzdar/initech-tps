453e4be51b90e2f26f63f62da8f86a2a2b2ecae5
viplav.fauzdar@zimidy.com <viplav.fauzdar@zimidy.com>
2020-04-01 19:13:44 -0400

    Adding test cases

---
+++ src/main/java/com/initech/tps/service/PhoneNumberMaskService.java
@@ -0,0 +1,13 @@
+package com.initech.tps.service;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+
+@Service
+@Slf4j
+public class PhoneNumberMaskService {
+
+    public String maskPhoneNumberWithDashes(String phoneNumber){
+        return null;
+    }
+}
--- src/test/java/com/initech/tps/InitechTpsApplicationTests.java
+++ src/test/java/com/initech/tps/InitechTpsApplicationTests.java
@@ -4,7 +4,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class TpsApplicationTests {
+public class InitechTpsApplicationTests {
 
 	@Test
 	void contextLoads() {
