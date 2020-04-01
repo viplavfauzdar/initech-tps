2a3f8310e1a3ebc11a12557b06a089b254cd817d
viplav.fauzdar@zimidy.com <viplav.fauzdar@zimidy.com>
2020-04-01 18:41:41 -0400

    Project configurations.

--- src/main/java/com/initech/tps/config/SwaggerConfig.java
+++ src/main/java/com/initech/tps/config/SwaggerConfig.java
@@ -1,14 +1,16 @@
 package com.initech.tps.config;
 
 
+import com.google.common.base.Predicates;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import springfox.documentation.builders.ApiInfoBuilder;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
-
 
 @Configuration
 @EnableSwagger2
@@ -17,9 +19,19 @@ public class SwaggerConfig {
     @Bean
     public Docket api() {
         return new Docket(DocumentationType.SWAGGER_2)
+                .apiInfo(apiInfo())
                 .select()
                 .apis(RequestHandlerSelectors.any())
                 .paths(PathSelectors.any())
+                .paths(Predicates.not(PathSelectors.regex("/error")))
+                .paths(Predicates.not(PathSelectors.regex("/forward")))
+                .build();
+    }
+
+    private ApiInfo apiInfo() {
+        return new ApiInfoBuilder()
+                .title("Initech TPS Report")
+                .description("THD code test")
                 .build();
     }
 
