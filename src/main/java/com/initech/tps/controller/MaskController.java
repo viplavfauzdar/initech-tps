5304417e1f8cbb919015abf658e341c230adc575
viplav.fauzdar@zimidy.com <viplav.fauzdar@zimidy.com>
2020-04-01 22:11:16 -0400

    Updated readme

--- src/main/java/com/initech/tps/controller/MaskController.java
+++ src/main/java/com/initech/tps/controller/MaskController.java
@@ -8,6 +8,10 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+/**
+ * MaskController class to mask email and phone numbers
+ * */
+
 @RestController
 @RequestMapping("/api")
 public class MaskController {
@@ -18,7 +22,12 @@ public class MaskController {
     @Autowired
     EmailMaskService emailMaskService;
 
-
+    /**
+     * REST Service for masking phone numbers by replacing the middle three digits with XXX
+     * e.g. 678-221-1212 returns 678-XXX-1212
+     *      678.221.1212 returns 678.XXX.1212
+     *      6782211212 returns 678XXX1212
+     * */
     @GetMapping("/maskphonenumber/{phonenumber}")
     public String maskPhoneNumber(@PathVariable String phonenumber){
         if(phonenumber.contains("-"))
@@ -29,6 +38,12 @@ public class MaskController {
             return phoneNumberMaskService.maskPhoneNumberWithTenDigits(phonenumber);
     }
 
+    /**
+     * REST Service method to mask email
+     * e.g. viplavF@gmail.com returns vXXXF@gmail.com
+     * @param - Email ID
+     * @return - Masked email ID
+     * */
     @GetMapping("/maskemailid/{emailid}")
     public String maskEmailId(@PathVariable String emailid){
         return emailMaskService.maskEmailId(emailid);
